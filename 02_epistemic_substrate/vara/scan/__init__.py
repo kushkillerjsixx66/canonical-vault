5ff1a5326ccca57c9f90d84bba37dfefab0f661e
Jeremy Sebastian <kushkillerjsixx66@gmail.com>
2026-06-26 13:34:09 -0400

    Create __init__.py

---
+++ 02_epistemic_substrate/vara/scan/__init__.py
@@ -0,0 +1,12 @@
+"""
+Canonical Vara Scan subsystem.
+
+Exports:
+- VaraScan
+- VaraScanEngine
+"""
+
+from .vara_scan import VaraScan
+from .vara_scan_engine import VaraScanEngine
+
+__all__ = ["VaraScan", "VaraScanEngine"]
