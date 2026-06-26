30844eaf7978bcca91337a3153c44c078bb6b58f
Jeremy Sebastian <kushkillerjsixx66@gmail.com>
2026-06-26 13:23:13 -0400

    Create __init__.py

---
+++ 05_runtime/veil/__init__.py
@@ -0,0 +1,12 @@
+"""
+Veil – Canonical Runtime Boundary Layer
+
+Exports:
+- Veil
+- VeilInterface
+"""
+
+from .veil_core import Veil
+from .veil_interface import VeilInterface
+
+__all__ = ["Veil", "VeilInterface"]
