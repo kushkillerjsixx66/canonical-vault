612be60508b522d8cd2756849ab611f31ef13dfb
Jeremy Sebastian <kushkillerjsixx66@gmail.com>
2026-06-26 13:56:12 -0400

    Create __init__.py

---
+++ 00_governance/stumpy/hooks/vara_scan/__init__.py
@@ -0,0 +1,10 @@
+"""
+Canonical Stumpy Governance Hooks for Vara Scan.
+
+Exports:
+- StumpyVaraScanHooks
+"""
+
+from .stumpy_vara_scan_hooks import StumpyVaraScanHooks
+
+__all__ = ["StumpyVaraScanHooks"]
